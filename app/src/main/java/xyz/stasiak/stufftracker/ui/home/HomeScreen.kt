e226653274926d1ddff2a4ad821937c4e3a69e67
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-04-28 15:47:36 +0200

    feat: Remove remember savable from showSearch

--- app/src/main/java/xyz/stasiak/stufftracker/ui/home/HomeScreen.kt
+++ app/src/main/java/xyz/stasiak/stufftracker/ui/home/HomeScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Top
 import androidx.compose.ui.Modifier
@@ -48,7 +47,7 @@ import xyz.stasiak.stufftracker.ui.theme.StuffTrackerTheme
 
 @Composable
 fun HomeScreen(navigateToItemUpdate: (Int) -> Unit, modifier: Modifier = Modifier) {
-    val showSearch = rememberSaveable { mutableStateOf(false) }
+    val showSearch = remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             StuffTrackerTopAppBar(
