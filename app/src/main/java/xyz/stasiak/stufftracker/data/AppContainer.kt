1d97356a3687377cbccbacc1bbc998db0ac0ff8c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-08 22:35:13 +0200

    feat: Create category table

--- app/src/main/java/xyz/stasiak/stufftracker/data/AppContainer.kt
+++ app/src/main/java/xyz/stasiak/stufftracker/data/AppContainer.kt
@@ -4,10 +4,15 @@ import android.content.Context
 
 interface AppContainer {
     val itemsRepository: ItemsRepository
+    val categoryRepository: CategoryRepository
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
     override val itemsRepository: ItemsRepository by lazy {
         OfflineItemsRepository(StuffTrackerDatabase.getDatabase(context).itemDao())
+    }
+
+    override val categoryRepository: CategoryRepository by lazy {
+        OfflineCategoryRepository(StuffTrackerDatabase.getDatabase(context).categoryDao())
     }
 }
---
+++ app/src/main/java/xyz/stasiak/stufftracker/data/Category.kt
@@ -0,0 +1,11 @@
+package xyz.stasiak.stufftracker.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "categories")
+data class Category(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+)
