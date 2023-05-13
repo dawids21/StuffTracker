4a460cca1b9623ed7b7010879c833aec693e20bf
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-13 12:03:18 +0200

    feat: Use content scale on details screen

--- app/src/main/java/xyz/stasiak/stufftracker/ui/product/details/ProductDetailsBodyContent.kt
+++ app/src/main/java/xyz/stasiak/stufftracker/ui/product/details/ProductDetailsBodyContent.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import xyz.stasiak.stufftracker.data.product.Product
 import xyz.stasiak.stufftracker.ui.product.ProductImage
 
 @Composable
 fun ProductDetailsBodyContent(product: Product) {
-    ProductImage(product.image, modifier = Modifier
-        .fillMaxWidth()
-        .aspectRatio(1f))
+    ProductImage(
+        product.image,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+    )
     ProductHeader(product)
     ProductParameters(product = product)
 }
