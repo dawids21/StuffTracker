1043642a36d4e6ab3d1fba97af7c366f56195bd9
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-16 21:44:38 +0200

    fix: Bugs

--- app/src/main/java/xyz/stasiak/stufftracker/ui/product/ProductImage.kt
+++ app/src/main/java/xyz/stasiak/stufftracker/ui/product/ProductImage.kt
@@ -1,14 +1,19 @@
 package xyz.stasiak.stufftracker.ui.product
 
-import androidx.compose.foundation.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Inventory2
-import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.core.net.toUri
-import coil.compose.rememberAsyncImagePainter
+import coil.compose.AsyncImage
 
 @Composable
 fun ProductImage(
@@ -16,17 +21,16 @@ fun ProductImage(
     modifier: Modifier = Modifier,
     contentScale: ContentScale = ContentScale.Fit
 ) {
-    imageUri?.let {
-        Image(
-            painter = rememberAsyncImagePainter(it.toUri()),
-            contentDescription = null,
-            contentScale = contentScale,
-            modifier = modifier
-        )
-    } ?: Icon(
-        Icons.Filled.Inventory2,
+    var loaded by remember { mutableStateOf(false) }
+    AsyncImage(
+        model = imageUri?.toUri(),
         contentDescription = null,
-//        contentScale = contentScale,
+        contentScale = contentScale,
+        fallback = rememberVectorPainter(image = Icons.Filled.Inventory2),
+        placeholder = rememberVectorPainter(image = Icons.Filled.Inventory2),
+        error = rememberVectorPainter(image = Icons.Filled.Inventory2),
+        onSuccess = { loaded = true },
         modifier = modifier,
+        colorFilter = if (!loaded) ColorFilter.tint(LocalContentColor.current) else null
     )
 }
