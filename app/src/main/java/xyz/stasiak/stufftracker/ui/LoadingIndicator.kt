2db45be30af62f2cd0a80848d388e487a6ca30f9
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-17 20:15:20 +0200

    feat: Use circular loading indicator

--- app/src/main/java/xyz/stasiak/stufftracker/ui/LoadingIndicator.kt
+++ app/src/main/java/xyz/stasiak/stufftracker/ui/LoadingIndicator.kt
@@ -3,12 +3,11 @@ package xyz.stasiak.stufftracker.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import xyz.stasiak.stufftracker.R
 
 @Composable
 fun LoadingIndicator(modifier: Modifier = Modifier) {
@@ -17,6 +16,6 @@ fun LoadingIndicator(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(R.string.loading))
+        CircularProgressIndicator(color = MaterialTheme.colorScheme.onPrimary)
     }
 }
