3641809c9d1d5f1cd5011144cd34dd766468239f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-18 19:37:49 +0200

    fix: Product image size

--- app/src/main/java/xyz/stasiak/stufftracker/ui/product/entry/ImageInput.kt
+++ app/src/main/java/xyz/stasiak/stufftracker/ui/product/entry/ImageInput.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -79,13 +81,14 @@ fun ImageInput(imageUri: String?, onValueChange: (Uri) -> Unit, modifier: Modifi
     Column(
         modifier = modifier
             .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         ProductImage(
             imageUri = imageUri,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .fillMaxWidth()
+                .size(300.dp)
                 .aspectRatio(1f)
         )
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
